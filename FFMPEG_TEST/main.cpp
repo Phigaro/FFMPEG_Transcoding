ecabf46a55855a790e2f8fdb5863600094277139
Phigaro <fkrlsp2@naver.com>
2017-01-12 15:06:00 +0900

    프로젝트 시작 및 비디오 이미지 스케일링 필터 적용

--- FFMPEG_TEST/main.cpp
+++ FFMPEG_TEST/main.cpp
@@ -18,6 +18,9 @@ extern "C"
 #include <libavutil/opt.h>
 #include <libavutil/imgutils.h>
 #include <libswscale/swscale.h>
+#include <libavfilter\avfilter.h>
+#include <libavfilter\buffersink.h>
+#include <libavfilter\buffersrc.h>
 
 #endif
 }
@@ -27,15 +30,128 @@ extern "C"
 #pragma comment( lib, "avformat.lib" )   
 #pragma comment( lib, "avutil.lib" )
 #pragma comment( lib, "avcodec.lib" )
-#pragma comment( lib, "swscale")
+#pragma comment( lib, "swscale.lib")
+#pragma comment( lib, "avfilter.lib")
+
+typedef struct _FilterContext {
+	AVFilterGraph* filter_graph;
+	AVFilterContext* src_ctx;
+	AVFilterContext* sink_ctx;
+} FilterContext;
+
+AVOutputFormat		*ofmt = NULL;
+AVFormatContext		*ifmt_ctx = NULL;
+AVFormatContext		*ofmt_ctx = NULL;
+
+static const int dst_width = 480;
+static const int dst_height = 320;
+
+
+static FilterContext vfilter_ctx, afilter_ctx;
+
+static int init_video_filter()
+{
+	AVStream* stream = ifmt_ctx->streams[0];
+	AVCodecContext* codec_ctx = stream->codec;
+	AVFilterContext* rescale_filter;
+	AVFilterInOut *inputs, *outputs;
+	char args[512];
+
+	vfilter_ctx.filter_graph = NULL;
+	vfilter_ctx.src_ctx = NULL;
+	vfilter_ctx.sink_ctx = NULL;
+
+	// Allocate memory for filter graph
+	vfilter_ctx.filter_graph = avfilter_graph_alloc();
+	if (vfilter_ctx.filter_graph == NULL)
+	{
+		return -1;
+	}
+
+	// Link input and output with filter graph.
+	if (avfilter_graph_parse2(vfilter_ctx.filter_graph, "null", &inputs, &outputs) < 0)
+	{
+		printf("Failed to parse video filtergraph\n");
+		return -2;
+	}
+
+	// Create input filter
+	// Create Buffer Source -> input filter
+	snprintf(args, sizeof(args), "time_base=%d/%d:video_size=%dx%d:pix_fmt=%d:pixel_aspect=%d/%d"
+		, stream->time_base.num, stream->time_base.den
+		, codec_ctx->width, codec_ctx->height
+		, codec_ctx->pix_fmt
+		, codec_ctx->sample_aspect_ratio.num, codec_ctx->sample_aspect_ratio.den);
+
+	// Create Buffer Source
+	if (avfilter_graph_create_filter(
+		&vfilter_ctx.src_ctx
+		, avfilter_get_by_name("buffer")
+		, "in", args, NULL, vfilter_ctx.filter_graph) < 0)
+	{
+		printf("Failed to create video buffer source\n");
+		return -3;
+	}
+
+	// Link Buffer Source with input filter
+	if (avfilter_link(vfilter_ctx.src_ctx, 0, inputs->filter_ctx, 0) < 0)
+	{
+		printf("Failed to link video buffer source\n");
+		return -4;
+	}
+
+	// Create output filter
+	// Create Buffer Sink
+	if (avfilter_graph_create_filter(
+		&vfilter_ctx.sink_ctx
+		, avfilter_get_by_name("buffersink")
+		, "out", NULL, NULL, vfilter_ctx.filter_graph) < 0)
+	{
+		printf("Failed to create video buffer sink\n");
+		return -3;
+	}
+
+	// Create rescaler filter to resize video resolution
+	snprintf(args, sizeof(args), "%d:%d", dst_width, dst_height);
+
+	if (avfilter_graph_create_filter(
+		&rescale_filter
+		, avfilter_get_by_name("scale")
+		, "scale", args, NULL, vfilter_ctx.filter_graph) < 0)
+	{
+		printf("Failed to create video scale filter\n");
+		return -4;
+	}
+
+	// link rescaler filter with aformat filter
+	if (avfilter_link(outputs->filter_ctx, 0, rescale_filter, 0) < 0)
+	{
+		printf("Failed to link video format filter\n");
+		return -4;
+	}
+
+	// aformat is linked with Buffer Sink filter.
+	if (avfilter_link(rescale_filter, 0, vfilter_ctx.sink_ctx, 0) < 0)
+	{
+		printf("Failed to link video format filter\n");
+		return -4;
+	}
+
+	// Configure all prepared filters.
+	if (avfilter_graph_config(vfilter_ctx.filter_graph, NULL) < 0)
+	{
+		printf("Failed to configure video filter context\n");
+		return -5;
+	}
+
+	av_buffersink_set_frame_size(vfilter_ctx.sink_ctx, codec_ctx->frame_size);
+
+	avfilter_inout_free(&inputs);
+	avfilter_inout_free(&outputs);
+}
 
 int main()
 {
-	AVOutputFormat		*ofmt = NULL;
-	AVFormatContext		*ifmt_ctx = NULL;
-	AVFormatContext		*ofmt_ctx = NULL;
-
-	//AVCodecID			v_codec_id	= AV_CODEC_ID_H264;
 	AVCodecID			v_codec_id = AV_CODEC_ID_H264;
 	AVCodecID			a_codec_id = AV_CODEC_ID_MP2;
 
@@ -44,9 +160,23 @@ int main()
 
 	av_register_all();
 
+	// 모든 필터를 등록함
+	avfilter_register_all();
+
+
 #ifdef _NETWORK
 	avformat_network_init();
 #endif
+
+	// ** 필터 생성 후 메모리 할당 ** //
+	AVFilterGraph *avfilter_graph_alloc(void);
+	void avfilter_graph_free(AVFilterGraph **graph);
+
+	AVFilterGraph *TempFilter;
+	TempFilter = avfilter_graph_alloc();
+
+	avfilter_graph_free(&TempFilter);
+	// ** 필터 생성 후 해제 //
 
 	int ret = 0;
 
@@ -68,7 +198,7 @@ int main()
 	//	avformat_alloc_output_context2(&ofmt_ctx, NULL, "mp4", outputfile);
 	/* ********************************************************************************** */
 
-	avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, outputfile);
+	avformat_alloc_output_context2(&ofmt_ctx, NULL, "mp4", outputfile);
 
 	if (!ofmt_ctx)
 	{
@@ -115,11 +245,14 @@ int main()
 	}
 
 	c->profile = FF_PROFILE_H264_BASELINE;
-	c->width = ifmt_ctx->streams[nVSI]->codec->width;
-	c->height = ifmt_ctx->streams[nVSI]->codec->height;
+	c->width = 480;
+	c->height = 320;
 	c->time_base = { 1,25 };
 	c->pix_fmt = ifmt_ctx->streams[nVSI]->codec->pix_fmt;
 	c->bit_rate = 50000;
+
+	//480;
+	//320;
 
 	if (avcodec_open2(c, codec, NULL) < 0)
 	{
@@ -217,6 +350,13 @@ int main()
 	pAFrame = av_frame_alloc();
 
 	av_init_packet(&outpkt);
+	
+	// 필터된 프레임이 저장될 곳;
+	AVFrame* filtered_frame = av_frame_alloc();
+
+	// 비디오 필터 설정
+	init_video_filter();
+	int stream_index;
 
 	while (av_read_frame(ifmt_ctx, &pkt) >= 0)
 	{
@@ -227,10 +367,43 @@ int main()
 			{
 				if (bGotPicture)
 				{
+					FilterContext* filter_ctx;
+
+					stream_index = pkt.stream_index;
+
+					if (stream_index == nVSI)
+					{
+						filter_ctx = &vfilter_ctx;
+						printf("[before] Video : resolution : %dx%d\n"
+							, pVFrame->width, pVFrame->height);
+					}
+					else
+					{
+						filter_ctx = &afilter_ctx;
+						printf("[before] Audio : sample_rate : %d / channels : %d\n"
+							, pVFrame->sample_rate, pVFrame->channels);
+					}
+
+					// put frame into filter.
+					if (av_buffersrc_add_frame(filter_ctx->src_ctx, pVFrame) < 0)
+					{
+						printf("Error occurred when putting frame into filter context\n");
+						break;
+					}
+
+					while (1)
+					{
+						// Get frame from filter, if it returns < 0 then filter is currently empty.
+						if (av_buffersink_get_frame(filter_ctx->sink_ctx, filtered_frame) < 0)
+						{
+							break;
+						}
+					}
+
 					outpkt.data = NULL;    // packet data will be allocated by the encoder
 					outpkt.size = 0;
 
-					ret			= avcodec_encode_video2(c, &outpkt, pVFrame, &bGotPicture);
+					ret			= avcodec_encode_video2(c, &outpkt, filtered_frame, &bGotPicture);
 					if (bGotPicture)
 					{
 						cout << "Write Frame [pts]=>" << outpkt.pts << " [size]=>" << outpkt.size << endl;
